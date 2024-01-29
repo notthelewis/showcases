0b0f99e43560f0fb44c9fcb99fcf385b29016ac5
wsl-11-2023 <lewfar99@gmail.com>
2024-01-29 01:44:36 +0000

    Added warning llabel to unsafe functions

--- simple-web-backend/utils/zero-alloc-conversions.go
+++ simple-web-backend/utils/zero-alloc-conversions.go
@@ -5,10 +5,12 @@ import (
 	"unsafe"
 )
 
+// The UNSAFE functions cannot be used if the returned data is to be modified in any way. A panic will occur.
 func BytesToStringUNSAFE(in []byte) string {
     return *(*string)(unsafe.Pointer(&in))
 }
 
+// The UNSAFE functions cannot be used if the returned data is to be modified in any way. A panic will occur.
 func StringToBytesUNSAFE(in string) (b []byte) {
     sliceHeader := (*reflect.SliceHeader)(unsafe.Pointer(&b))
     strHeader := *(*reflect.StringHeader)(unsafe.Pointer(&in))
