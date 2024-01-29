e36afcf6659f666a99a23ce8d4aa2a616df2df4e
wsl-11-2023 <lewfar99@gmail.com>
2024-01-29 01:50:19 +0000

    remove deprecated error

--- simple-web-backend/typed-errors/typed-errors.go
+++ simple-web-backend/typed-errors/typed-errors.go
@@ -2,7 +2,6 @@ package typederrors
 
 import "errors"
 
-var ErrUnableToGetPoolEntry = errors.New("FATAL - Unable to get a new log pool entry")
-
 var Err404 = errors.New("Page not found")
 var Err500 = errors.New("Internal server error")
+var ErrNoPageData = errors.New("Invalid page data retreival")
