f66f188a71f7544f642454d5ea1a58855207d392
wsl-11-2023 <lewfar99@gmail.com>
2024-03-17 00:23:49 +0000

    feat(decoder): Add bool support

--- rust/blewis/src/data_type.rs
+++ rust/blewis/src/data_type.rs
@@ -1,4 +1,6 @@
-use bytes::Bytes;
+use std::{io::Read, u16};
+
+use bytes::{Buf, BufMut, Bytes};
 
 #[derive(Debug, PartialEq)]
 pub(crate) enum DataType {
@@ -22,8 +24,30 @@ pub(crate) enum Int {
 
 #[derive(Debug, PartialEq)]
 pub(crate) struct Error {
+    pub is_server_err: bool,
     pub err_code: u8,
-    pub err_msg: Bytes,
+    // Store error message as byte fields in standard ascii (for now)
+    pub err_msg: Bytes
+}
+
+impl Error {
+    pub fn encode(&self) -> bytes::BytesMut {
+        let mut to_return = bytes::BytesMut::with_capacity(self.err_msg.len() + 5);
+
+        to_return.put_u8(0b_0_0000_110);
+
+        if self.is_server_err {
+            to_return.put_u8(1);
+        } else {
+            to_return.put_u8(0);
+        }
+
+        to_return.put_u8(self.err_code);
+        to_return.put_u16(self.err_msg.len() as u16);
+        to_return.put(self.err_msg.as_ref());
+
+        to_return
+    }
 }
 
 #[derive(Debug, PartialEq)]
