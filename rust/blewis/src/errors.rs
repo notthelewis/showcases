e61315c1906fbd4bd18fc4e007884bc0560d0907
wsl-11-2023 <lewfar99@gmail.com>
2024-03-21 20:40:29 +0000

    feat(errors): Add typed errors

---
+++ rust/blewis/src/errors.rs
@@ -0,0 +1,10 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum DecodeError {
+    #[error("Unable to decode {0} from buffer as buffer's length is too small")]
+    BufTooShortError(&'static str),
+
+    #[error("Unable to decode buffer due to unknown meta byte: {0}")]
+    UnknownMetaByte(u8)
+}
