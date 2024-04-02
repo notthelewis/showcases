6f9f5cafd6757b06653cc259148b5c7236ea7aa6
wsl-11-2023 <lewfar99@gmail.com>
2024-04-02 23:11:52 +0100

    feat(store): Cohesive return type and get_del cmd
    Add get_del command and make return type cohesive for the `set` command.

--- rust/blewis/src/store.rs
+++ rust/blewis/src/store.rs
@@ -1,5 +1,5 @@
 use crate::{data_type::DataType, errors::GETSET_KEY_NO_EXIST};
-use dashmap::{mapref::one, DashMap};
+use dashmap::DashMap;
 use std::sync::Arc;
 
 /// Store is the concurrent hashmap that is the core of `Blewis`
@@ -10,12 +10,17 @@ impl Store {
         Store(Arc::new(DashMap::new()))
     }
 
-    pub fn get(&self, key: &DataType) -> Option<one::Ref<DataType, DataType>> {
-        self.0.get(key)
+    /// Retrieves a value from the store
+    pub fn get(&self, key: &DataType) -> Option<DataType> {
+        let res = self.0.get(key);
+        match res {
+            Some(v) => Some(v.to_owned()),
+            None => None
+        }
     }
 
     /// Simillar to set but instead of returning an Option<DataType>, it returns an error code if
-    /// a previous value was not set 
+    /// a previous value was not set
     pub fn get_set(&self, key: &DataType, new_val: &DataType) -> DataType {
         let existing_val = self.0.insert(key.to_owned(), new_val.to_owned());
 
@@ -26,18 +31,24 @@ impl Store {
         existing_val.unwrap().to_owned()
     }
 
+    pub fn get_del(&self, key: &DataType) -> Option<DataType> {
+        match self.0.remove(key) {
+            Some((_, entry)) => Some(entry),
+            None => None,
+        }
+    }
+
     /// When the set command is ran, if a value with the key already exists, it replaces it and returns
     /// the old value
     pub fn set(&self, key: &DataType, value: &DataType) -> Option<DataType> {
         self.0.insert(key.to_owned(), value.to_owned())
     }
-
 }
 
 mod test {
     #![allow(unused_imports)]
 
-    use super::Store; 
+    use super::Store;
     use crate::{
         data_type::{BoopBool, BoopError, BoopString, DataType, Int},
         errors::GETSET_KEY_NO_EXIST,
@@ -54,7 +65,7 @@ mod test {
 
         let set_res = store.set(&key, &val);
         assert!(set_res.is_none());
-        assert_eq!(*store.get(&key).unwrap(), val);
+        assert_eq!(store.get(&key).unwrap(), val);
     }
 
     #[test]
@@ -78,5 +89,18 @@ mod test {
             ),
             GETSET_KEY_NO_EXIST
         );
+    }
+
+    #[test]
+    fn getdel_entry_exists() {
+        let store: Store = Store::new();
+        let key = BoopString::new(Bytes::from_static(b"getset"));
+        let val = BoopBool::new(true);
+
+        store.0.insert(key.clone(), val.clone());
+
+        assert_eq!(store.get_del(&key.clone()).unwrap(), val);
+        assert!(store.get_del(&key.clone()).is_none());
+        assert!(store.get(&key).is_none())
     }
 }
