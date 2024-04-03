ce111c85324da3b7eab049184538afdd33a52c22
wsl-11-2023 <lewfar99@gmail.com>
2024-04-03 14:20:12 +0100

    refactor(scope): Remove GETSET_KEY_NO_EXIST

--- rust/blewis/src/errors.rs
+++ rust/blewis/src/errors.rs
@@ -11,10 +11,3 @@ pub enum DecodeError {
     #[error("Unable to decode buffer due to unknown meta byte: {0}")]
     UnknownMetaByte(u8),
 }
-
-/// GETSET_KEY_NO_EXIST is emitted by the GetSet command.
-pub const GETSET_KEY_NO_EXIST: DataType = DataType::Error(BoopError {
-    is_server_err: true,
-    err_code: 0x10,
-    err_msg: Bytes::from_static(b"no_exist"),
-});
