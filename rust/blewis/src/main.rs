159db9ef733e39661234cac48a11d5c92d45c877
wsl-11-2023 <lewfar99@gmail.com>
2024-04-03 15:33:06 +0100

    feat(main): basic mainloop

--- rust/blewis/src/main.rs
+++ rust/blewis/src/main.rs
@@ -1,4 +1,7 @@
-mod command_interpreter;
+use network::tcp;
+use store::Store;
+
+mod command;
 mod data_type;
 mod decoder;
 mod encoder;
@@ -6,4 +9,11 @@ mod errors;
 mod network;
 mod store;
 
-fn main() {}
+fn main() -> anyhow::Result<()> {
+    let store = Store::new();
+
+    let mut tcp_server = tcp::TCPServer::new("127.0.0.1:1523", store.clone())?;
+    tcp_server.run()?;
+
+    Ok(())
+}
