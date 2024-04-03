4272c3cec1974721cca47933c205f0ecc2cfd437
wsl-11-2023 <lewfar99@gmail.com>
2024-04-03 15:39:47 +0100

    chore(inline-removal): Remove inline from constructor

--- rust/blewis/src/store.rs
+++ rust/blewis/src/store.rs
@@ -11,24 +11,22 @@ use std::sync::Arc;
 pub struct Store(Arc<DashMap<DataType, DataType>>);
 
 impl Store {
-    #[inline(always)]
     pub fn new() -> Self {
         Store(Arc::new(DashMap::new()))
     }
 
+    #[inline]
     pub fn clone(&self) -> Self {
         Store(self.0.clone())
     }
 
     /// Creates a new Store with a preset capacity
-    #[inline(always)]
     pub fn with_capacity(cap: usize) -> Self {
         Store(Arc::new(DashMap::with_capacity(cap)))
     }
 
     /// Creates a new Store with a preset capacity and shard amount. The shard amount must be a
     /// power of two. If a none power of two is selected, the program will panic.
-    #[inline(always)]
     pub fn with_capacity_and_shard_amount(cap: usize, shard_amount: usize) -> Self {
         Store(Arc::new(DashMap::with_capacity_and_shard_amount(
             cap,
